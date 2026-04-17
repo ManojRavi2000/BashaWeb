7c1668502bbe02d597f981413161e38264915cd9
ManojRavi2000 <110084456+ManojRavi2000@users.noreply.github.com>
2026-04-17 11:22:29 +0530

    update

--- BashaBhai/src/app/Environment/environment.ts
+++ BashaBhai/src/app/Environment/environment.ts
@@ -1,10 +1,10 @@
 export const environment = {
      production: true,
-    API_URL: 'http://api.basha-bhai.com:8081',
+    // API_URL: 'http://api.basha-bhai.com:8081',
     
     //  API_URL: 'http://localhost:8090/',
     // API_URL1: 'http://localhost:8081/poltech',
-    // API_URL:'http://65.0.85.4:8090/'
+    // API_URL:'http://145.223.19.113:8081/restaurant'
 
-    // API_URL: 'http://localhost:8081/restaurant',
+     API_URL: 'https://app.basha-bhai.com',
 };
